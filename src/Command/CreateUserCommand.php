b7145d6dcaee9e7d954478a1234524fd9990f29b
Fabien CURFS <fabien.curfs@etudiant.univ-reims.fr>
2023-03-14 16:06:52 +0100

    Create UserCommand

---
+++ src/Command/CreateUserCommand.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Command;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\Question;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+// the name of the command is what users type after "php bin/console"
+#[AsCommand(
+    name: 'app:create-admin',
+    description: 'Creates a admin user.',
+)]
+class CreateUserCommand extends Command
+{
+    private SymfonyStyle $io;
+    protected EntityManagerInterface $entityManager;
+    protected UserPasswordHasherInterface $passwordHasher;
+
+    public function __construct(EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->entityManager = $entityManager;
+        $this->passwordHasher = $passwordHasher;
+
+        parent::__construct();
+    }
+
+    protected function initialize(InputInterface $input, OutputInterface $output): void
+    {
+        $this->io = new SymfonyStyle($input, $output);
+    }
+
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $user = new User();
+        $user->setEmail('admin@admin.fr');
+        $user->setRoles(['ROLE_ADMIN']);
+        //$user->setRoles(['ROLE_ADMIN', 'ROLE_USER']);
+
+        $hashedPassword = $this->passwordHasher->hashPassword($user, 'toto1234');
+        $user->setPassword($hashedPassword);
+
+        $this->entityManager->persist($user);
+        $this->entityManager->flush();
+
+        $this->io->success("Admin user created !");
+        $this->io->text('admin@admin.fr / toto1234');
+
+        return Command::SUCCESS;
+
+        /*$helper = $this->getHelper('question');
+        $questionLogin = new Question('username ?');
+        $questionPassword = new Question('password ?');
+        $questionPassword->setHidden(true);
+        $questionPassword->setHiddenFallback(false);*/
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->setHelp('This command allows you to create a user...');
+    }
+}

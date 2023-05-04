23f27bd6546695eeca8f97804bc3119d6e19b1ae
Fabien CURFS <fabien.curfs@etudiant.univ-reims.fr>
2023-05-04 08:09:34 +0200

    User entity

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -10,6 +10,7 @@ use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ApiResource(
@@ -19,7 +20,8 @@ use Symfony\Component\Security\Core\User\UserInterface;
             controller: UserController::class,
             name: 'api_users_me'
         )
-    ]
+    ],
+    normalizationContext: ['groups' => ['read:user']],
 )]
 #[ORM\Table(name: '`user`')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
@@ -27,12 +29,15 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['read:user'])]
     private ?int $id = null;
 
     #[ORM\Column(length: 180, unique: true)]
+    #[Groups(['read:user'])]
     private ?string $email = null;
 
     #[ORM\Column]
+    #[Groups(['read:user'])]
     private array $roles = [];
 
     /**
